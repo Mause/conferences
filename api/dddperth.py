51c4329a68de889236dd062586de6b68bd878703
Elliana May <me@mause.me>
2022-09-10 13:02:11 +0800

    chore: fix 404?

--- api/dddperth.py
+++ api/dddperth.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api/dddperth/')
 def schedule():
     data = requests.get('https://dddperth-functions-prod.azurewebsites.net/api/GetAgenda').json()
     return jsonify(data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return request.url + ' not found'
 
