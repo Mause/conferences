42261a22a68310a35f85cfbdbbd057c27e507d98
Elliana May <me@mause.me>
2022-09-10 14:12:54 +0800

    the rest of the owl

--- api/dddperth.py
+++ api/dddperth.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, render_template
 import requests
-from datetime import date
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 
-start_date = end_date = date(2022, 9, 10)
+start_date = end_date = datetime(2022, 9, 10, 0, 0, 0)
 
 
+def to_time(t):
+    fmt = t['Format']
+    if not (fmt and fmt[0].isdigit()):
+        return '00:00'
+    return '{:02d}:{:02d}'.format(*divmod(int(fmt.split()[0]), 60))
 
 @app.route('/api/dddperth/')
 def schedule():
-    data = requests.get('https://dddperth-functions-prod.azurewebsites.net/api/GetAgenda').json()
+    data = requests.get('https://dddperth-functions-prod.azurewebsites.net/api/GetAgenda', verify=False).json()
     return (
         render_template(
             "schedule.xml",
-            days={start_date: {'room': data}},
-            to_time=lambda t: '{:02d}:{:02d}'.format(*divmod(t, 60)),
+            days={start_date: {'room': [{'start': start_date, 'end': end_date, **talk} for talk in data]}},
+            to_time=to_time,
             start_date=start_date,
             end_date=end_date,
         ),
