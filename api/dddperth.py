be943ffd318abbd7f9b3330dbd7f89592f0f34a7
Elliana May <me@mause.me>
2022-09-10 13:12:35 +0800

    chore: try to render

--- api/dddperth.py
+++ api/dddperth.py
@@ -1,12 +1,30 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, render_template
 import requests
+from datetime import date
+
 
 app = Flask(__name__)
+
+
+start_date = end_date = date(2022, 9, 10)
+
+
 
 @app.route('/api/dddperth/')
 def schedule():
     data = requests.get('https://dddperth-functions-prod.azurewebsites.net/api/GetAgenda').json()
-    return jsonify(data)
+    return (
+        render_template(
+            "schedule.xml",
+            days=data,
+            to_time=lambda t: '{:02d}:{:02d}'.format(*divmod(t, 60)),
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        200,
+        {"content-type": "application/xml"},
+    )
+
 
 @app.errorhandler(404)
 def page_not_found(e):
