be514c009a3b50652722dc38a4d1924726d93281
Elliana May <me@mause.me>
2026-08-27 09:52:19 +1000

    slash

--- api/pycon2026/shed.py
+++ api/pycon2026/shed.py
@@ -35,7 +35,7 @@ def schedule_year_xml(year):
     return get_schedule()
 
 
-@app.route("*")
+@app.route("/*")
 def catch_all(path):
     return Response(f"Hello from {path} || {request.url}", status=200)
 
